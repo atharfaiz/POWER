405f5e7b9f2687fe7fe5c07556a7306bd1f58eef
atharfaiz <mdfaizathar@gmail.com>
2021-09-18 19:44:13 +0530

    created first endpoint

--- PowerServer/src/main/java/org/fz/powerapp/powerserver/controller/DashboardController.java
+++ PowerServer/src/main/java/org/fz/powerapp/powerserver/controller/DashboardController.java
@@ -1,10 +1,15 @@
 package org.fz.powerapp.powerserver.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/dashboard/api")
 @RestController
 public class DashboardController {
 	
+	@GetMapping("/message")
+	public String getMessage() {
+		return "this is my dashboard";
+	}
 
 }
